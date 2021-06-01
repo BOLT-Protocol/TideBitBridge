0ebe5b9fb36ac426b7bba5a840b366342babc644
TzuHanLiang <crocodilestear@gmail.com>
2021-06-01 09:27:01 +0800

    feat: add simple sass and webpack.config

---
+++ src/frontend/assets/javascript/views.js
@@ -0,0 +1,5 @@
+const elements = {
+  assetSelector: document.querySelector("#asset-selector"),
+};
+
+elements.assetSelector.addEventListener("click", (evt) => popupAssetList(evt));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
-const webpack = require('webpack');
-const fs = require('fs');
-const path = require('path');
+const webpack = require("webpack");
+const fs = require("fs");
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== "production";
 
-function getExternals()
-{
-  const nodeModules = fs.readdirSync(path.join(process.cwd(), 'node_modules'));
+function getExternals() {
+  const nodeModules = fs.readdirSync(path.join(process.cwd(), "node_modules"));
   return nodeModules.reduce((ext, mod) => {
     ext[mod] = `commonjs ${mod}`;
     return ext;
@@ -12,52 +15,85 @@ function getExternals()
 }
 
 const frontend = {
-  mode: 'none',
-  entry: path.resolve(__dirname, 'src/frontend/main.js'),
+  mode: "none",
+  entry: path.resolve(__dirname, "src/frontend/main.js"),
   output: {
-    path: path.resolve(__dirname, 'build/frontend'),
-    filename: 'main.js',
-    chunkFilename: '[id].js'
+    path: path.resolve(__dirname, "build/frontend"),
+    filename: "main.js",
+    chunkFilename: "[id].js",
+    // Where the CSS is saved to
+    publicPath: "/css",
+  },
+  resolve: {
+    extensions: [".css", ".scss"],
+    alias: {
+      // Provides ability to include node_modules with ~
+      "~": path.resolve(process.cwd(), "src", "scss"),
+    },
   },
   module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: { plugins: [autoprefixer(), cssnano()] },
+              publicPath: '../',
+            },
+          },
+          "sass-loader",
+          
+        ],
+      },
+    ],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ]
+    new webpack.NoEmitOnErrorsPlugin(),
+    new MiniCssExtractPlugin({
+      filename: devMode ? "[name].css" : "[name].[contenthash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[contenthash].css",
+    }),
+    // only enable hot in development
+    devMode ? new webpack.HotModuleReplacementPlugin() : {},
+  ],
 };
 
 const backend = {
-  mode: 'none',
-  target: 'node',
-  entry: path.resolve(__dirname, 'src/backend/main.js'),
+  mode: "none",
+  target: "node",
+  entry: path.resolve(__dirname, "src/backend/main.js"),
   output: {
-    path: path.resolve(__dirname, 'build/backend'),
-    filename: 'main.js',
-    chunkFilename: '[id].js'
+    path: path.resolve(__dirname, "build/backend"),
+    filename: "main.js",
+    chunkFilename: "[id].js",
   },
   externals: getExternals(),
   node: {
     __filename: true,
-    __dirname: true
+    __dirname: true,
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /(node_modules)/
-    }],
-    exprContextCritical: false
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /(node_modules)/,
+      },
+    ],
+    exprContextCritical: false,
   },
   plugins: [
     new webpack.IgnorePlugin(/\.(css|less|scss|svg|png|jpe?g|png)$/),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
-      debug: false
-    })
-  ]
-}
+      debug: false,
+    }),
+  ],
+};
 
-module.exports = [
-  frontend
-];
+module.exports = [frontend];
