081b0b6cb7199aa1d18e0798cdf4bfbc6ea20d2a
TzuHanLiang <crocodilestear@gmail.com>
2021-06-04 10:21:15 +0800

    feat: update rwd

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'bin/main.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : 'ubuntu@ec2-3-34-43-9.ap-northeast-2.compute.amazonaws.com',
+      ref  : 'origin/feature/pannel-control', //-- change to develop
+      repo : 'https://github.com/BOLT-Protocol/TideBitBridge.git',
+      path : '/etc/TideBitBridge',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env develop'
+    }
+  }
+};
