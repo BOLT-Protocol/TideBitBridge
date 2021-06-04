ab00fa8274bee3a9bebd92bd633c6f8100588ca3
TzuHanLiang <crocodilestear@gmail.com>
2021-06-04 10:56:57 +0800

    feat: eco config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,27 +1,38 @@
 module.exports = {
-  apps : [{
-    name: 'API',
-    script: 'bin/main.js',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
+  apps: [
+    {
+      name: "API",
+      script: "bin/main.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
     },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }],
-
-  deploy : {
-    production : {
-      user : 'ubuntu',
-      host : 'ubuntu@ec2-3-34-43-9.ap-northeast-2.compute.amazonaws.com',
-      ref  : 'origin/feature/pannel-control', //-- change to develop
-      repo : 'https://github.com/BOLT-Protocol/TideBitBridge.git',
-      path : '/etc/TideBitBridge',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env develop'
-    }
-  }
+  ],
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "ec2-3-34-43-9.ap-northeast-2.compute.amazonaws.com",
+      ref: "origin/feature/develop", //-- change to productions
+      repo: "https://github.com/BOLT-Protocol/TideBitBridge.git",
+      path: "/etc/TideBitBridge",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+    },
+    staging: {
+      user: "ubuntu",
+      host: "ec2-3-34-43-9.ap-northeast-2.compute.amazonaws.com",
+      ref: "origin/feature/pannel-control", //-- change to development
+      repo: "https://github.com/BOLT-Protocol/TideBitBridge.git",
+      path: "/etc/TideBitBridge",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env development",
+    },
+  },
 };
